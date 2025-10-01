45682f3925a334a1ff44eaeaf5b6c744f4421d41
kkthenuttah-debug <kkthenuttah@gmail.com>
2025-10-01 02:27:20 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 5001;
-
-app.get("/", (req, res) => res.send("Hello from sample-node-app!"));
-
-app.listen(port, () => console.log(`Listening on ${port}`));
