2570bff9e64107ecd0408470c75a4f4546b98c4a
kkthenuttah-debug <kkthenuttah-debug@users.noreply.github.com>
2025-10-01 06:22:00 +0100

    Add new project via Project Generator

---
+++ oac4ognr6/port-test/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/node-apps-oac4ognr6-port-test/',
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  }
+})
