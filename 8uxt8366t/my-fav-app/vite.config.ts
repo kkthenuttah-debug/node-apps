ba8a1993a5b44bca475f948b80ecc7112ace35e8
kkthenuttah-debug <kkthenuttah-debug@users.noreply.github.com>
2025-10-02 16:56:58 +0100

    Add new project via Project Generator

---
+++ 8uxt8366t/my-fav-app/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/node-apps-8uxt8366t-my-fav-app/',
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  }
+})
